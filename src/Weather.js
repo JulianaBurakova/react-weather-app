22203e05af42f4393400a610c08e5785a46f9072
Juliana8b <167132725+JulianaBurakova@users.noreply.github.com>
2024-06-10 13:19:14 -0500

    add basic css, logo

--- src/Weather.js
+++ src/Weather.js
@@ -30,12 +30,25 @@ export default function Weather() {
 
   let form = (
     <div className="Weather">
+      <a
+        href="https://juliana-portfolio-web.netlify.app"
+        target="_blank"
+        rel="noopener noreferrer">
+        <img
+          className="image-fluid"
+          src="/image/MyLogo1.png"
+          class="logo"
+          width={150}
+          alt="Juliana Logo"
+        />
+      </a>
       <form onSubmit={handleSearch}>
         <div className="row">
           <div className="col-9 ">
             <input
               type="search"
               placeholder="Enter a city..."
+              className="form-control"
               onChange={updateSearch}
             />
           </div>
@@ -44,6 +57,33 @@ export default function Weather() {
           </div>
         </div>
       </form>
+      <div className="row mt-5">
+        <div className="col-6">
+          <h1>Paris</h1>
+          <ul>
+            <li>
+              <span>Sunday 18:52</span>, few clouds
+            </li>
+            <li>
+              Humidity: <strong>78%</strong>, Wind: <strong>12.07km/h</strong>
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-8">
+          <div class="temperature-container d-flex justify-content-end">
+            <img
+              id="dimg_15"
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII="
+              class="weather-img"
+              alt="Partly sunny"
+            />
+            <div>
+              <span class="temperature">16</span>
+              <span class="unit">°C |°F</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <footer>
         This project was coded by{" "}
         <a
